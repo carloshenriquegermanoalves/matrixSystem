59c7dfcb38427232f7bf8664da80f324a70d0f3e
carloshenriquegermanoalves <carloshenriquecoding023@gmail.com>
2023-10-09 20:42:47 -0300

    Implementando métodos de cofatores e determinantes.

--- src/main/java/org/matrix/System.java
+++ src/main/java/org/matrix/System.java
@@ -2,13 +2,14 @@ package org.matrix;
 
 public interface System {
 
-    boolean isPossibleAddOrSubtract(Matrix matrixA, Matrix matrixB);
+    boolean isPossibleAddOrSubtractOrMultiply(Matrix matrixA, Matrix matrixB);
     Matrix addingMatrix(Matrix matrixA, Matrix matrixB);
     Matrix subtractingMatrix(Matrix matrixA, Matrix matrixB);
     Matrix scalarMultiplying(int scalar, Matrix matrix);
     Matrix multiplyingMatrix(Matrix matrixA, Matrix matrixB);
     Matrix transposingMatrix(Matrix matrix);
-    Matrix determinantMatrix(Matrix matrix);
+    double determinantMatrix(Matrix matrix);
+    double cofactorMatrix(Matrix matrix, int row, int col);
 
 
 }
--- src/main/java/org/matrix/SystemImplementation.java
+++ src/main/java/org/matrix/SystemImplementation.java
@@ -1,22 +1,18 @@
 package org.matrix;
 
 public class SystemImplementation implements System {
-
-    public boolean isPossibleAddOrSubtract(Matrix matrixA, Matrix matrixB) {
-        return matrixA.getCols() == matrixB.getRows();
+    @Override
+    public boolean isPossibleAddOrSubtractOrMultiply(Matrix matrixA, Matrix matrixB) {
+        return matrixA.getCols() != matrixB.getRows();
     }
 
-
     public Matrix addingMatrix(Matrix matrixA, Matrix matrixB) {
-        if (!isPossibleAddOrSubtract(matrixA, matrixB))
+        if (isPossibleAddOrSubtractOrMultiply(matrixA, matrixB))
             throw new IllegalArgumentException("As matrizes não podem ser somadas, por terem dimensões incompatíveis!");
 
-        int rows = matrixA.getRows();
-        int cols = matrixA.getCols();
-        Matrix result = new Matrix(rows, cols);
-
-        for(int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        Matrix result = new Matrix(matrixA.getRows(), matrixA.getCols());
+        for(int i = 0; i < matrixA.getRows(); i++) {
+            for (int j = 0; j < matrixA.getCols(); j++) {
                 double sum = matrixA.getPosition(i, j) + matrixB.getPosition(i, j);
                 result.setMatrix(i, j, sum);
             }
@@ -27,11 +23,10 @@ public class SystemImplementation implements System {
 
     @Override
     public Matrix subtractingMatrix(Matrix matrixA, Matrix matrixB) {
-        if (!isPossibleAddOrSubtract(matrixA, matrixB))
+        if (isPossibleAddOrSubtractOrMultiply(matrixA, matrixB))
             throw new IllegalArgumentException("As matrizes não podem ser subtraídas, por terem dimensões incompatíveis");
 
         Matrix result = new Matrix(matrixA.getRows(), matrixA.getCols());
-
         for (int i = 0; i < matrixA.getRows(); i++) {
             for(int j = 0; j < matrixA.getCols(); j++) {
                double sub = matrixA.getPosition(i, j) - matrixB.getPosition(i, j);
@@ -45,10 +40,25 @@ public class SystemImplementation implements System {
     @Override
     public Matrix scalarMultiplying(int scalar, Matrix matrix) {
         Matrix result = new Matrix(matrix.getRows(), matrix.getCols());
-
         for (int i = 0; i < matrix.getRows(); i++) {
             for (int j = 0; j < matrix.getCols(); j++) {
-                double mult = scalar * matrix.getPosition(i, j);
+                double scal = scalar * matrix.getPosition(i, j);
+                result.setMatrix(i, j, scal);
+            }
+        }
+        return result;
+    }
+
+
+    @Override
+    public Matrix multiplyingMatrix(Matrix matrixA, Matrix matrixB) {
+        if (isPossibleAddOrSubtractOrMultiply(matrixA, matrixB))
+            throw new IllegalArgumentException("As matrizes não podem ser multiplicadas, pois possuem dimensões diferentes!");
+
+        Matrix result = new Matrix(matrixA.getRows(), matrixA.getCols());
+        for (int i = 0; i < matrixA.getRows(); i++) {
+            for (int j = 0; j < matrixA.getCols(); j++) {
+                double mult = matrixA.getPosition(i, j) * matrixB.getPosition(i, j);
                 result.setMatrix(i, j, mult);
             }
         }
@@ -56,21 +66,56 @@ public class SystemImplementation implements System {
     }
 
 
-
-    @Override
-    public Matrix multiplyingMatrix(Matrix matrixA, Matrix matrixB) {
-        return null;
-    }
-
-
     @Override
     public Matrix transposingMatrix(Matrix matrix) {
-        return null;
+        Matrix transposed = new Matrix(matrix.getCols(), matrix.getRows());
+        for (int i = 0; i < transposed.getRows(); i++) {
+            for (int j = 0; j < transposed.getCols(); j++) {
+                double elementPosition = matrix.getPosition(i, j);
+                transposed.setMatrix(j, i, elementPosition);
+            }
+        }
+        return transposed;
     }
 
     @Override
-    public Matrix determinantMatrix(Matrix matrix) {
-        return null;
+    public double determinantMatrix(Matrix matrix) {
+        if (matrix.getRows() == 1 && matrix.getCols() == 1)
+            return matrix.getPosition(0, 0);
+        else {
+            double determinant;
+            if (matrix.getRows() == 2 && matrix.getCols() == 2) {
+                determinant = (matrix.getPosition(1, 1) * matrix.getPosition(2, 2)) -
+                        (matrix.getPosition(1, 2) * matrix.getPosition(2, 1));
+            } else {
+                determinant = 0.0;
+                for (int col = 0; col < matrix.getCols(); col++) {
+                    double cofactor = matrix.getPosition(0, col) * cofactorMatrix(matrix, 0, col);
+                    determinant += (col % 2 == 0 ? 1 : -1) * cofactor;
+                }
+            }
+            return determinant;
+        }
+    }
+
+    @Override
+    public double cofactorMatrix(Matrix matrix, int row, int col) {
+        Matrix subMatrix = new Matrix(matrix.getRows() - 1, matrix.getCols() - 1);
+
+        for (int i = 0, subRow = 0; i < matrix.getRows(); i++) {
+            if (i == row) {
+                continue;
+            }
+            for (int j = 0, subCol = 0; j < matrix.getCols(); j++) {
+                if (j == col) {
+                    continue;
+                }
+                subMatrix.setMatrix(subRow, subCol, matrix.getPosition(i, j));
+                subCol++;
+            }
+            subRow++;
+        }
+        return determinantMatrix(subMatrix);
     }
 
 }
